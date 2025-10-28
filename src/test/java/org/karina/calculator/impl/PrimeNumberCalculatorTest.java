8e5bf364c37e6347aeec484589da9d9d752be346
Karina Talibzhanova <karina.talibzhanova@gmail.com>
2025-10-28 14:37:38 +0000

    add skeleton prime number unit test

---
+++ src/test/java/org/karina/calculator/impl/PrimeNumberCalculatorTest.java
@@ -0,0 +1,31 @@
+package org.karina.calculator.impl;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.karina.calculator.PrimeNumberCalculator;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PrimeNumberCalculatorTest {
+
+    @ParameterizedTest
+    @MethodSource("generator")
+    void testCalculator(PrimeNumberCalculator calculator, int n, List<Integer> expected) {
+
+        List<Integer> result = calculator.calculate(n);
+
+        assertEquals(expected, result);
+    }
+
+    private static Stream<Arguments> generator() {
+        // get test data
+
+        return Stream.of(
+                Arguments.of(new SieveOfEratosthenes(), 10, List.of(2,3,5,7))
+        );
+    }
+}
